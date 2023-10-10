f81a2fc0d74ecec12a91dc4cc8072954dcc03f03
Tsiprovskiy Egor <egor-ciprov@mail.ru>
2023-10-10 21:33:22 +0300

    добавлен файл таймера

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import requestCall from "./modules/requestCall";
 import slaider from "./modules/slaider";
 import call from "./modules/modalCall";
+import timer from "./modules/timer";
 
-requestCall()
-slaider()
-call()
+requestCall();
+slaider();
+call();
+timer();
