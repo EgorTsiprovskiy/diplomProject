04c4ea4b7540114916a14ded89928fb7c45e7a15
Tsiprovskiy Egor <egor-ciprov@mail.ru>
2023-10-13 06:22:33 +0300

    исправлена работа скролла вверх страницы

--- src/modules/scrollTop.js
+++ src/modules/scrollTop.js
@@ -1,7 +1,16 @@
 const scroll = () => {
-    document.querySelector('.smooth-scroll').onclick = () => {
-        window.scrollTo(pageYOffset, 0);
+let scrollButton = document.querySelector(".smooth-scroll");
+scrollButton.style.display = "none";
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 700) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
     }
-}
+  });
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+};
 
-export default scroll
+export default scroll;
